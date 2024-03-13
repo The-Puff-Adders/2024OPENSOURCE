05ebc16381f65937b1e96188a0c15d20942ec5e3
jmngandu <jamesmngandu@gmail.com>
2024-03-13 13:02:59 +0300

    commit

---
+++ ChepBay/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from './Components/Nav'
+import Hero from './Components/Hero'
+import Service from './Service'
+import data from './Components/data'
+
+function App() {
+
+  return (
+    <>
+     <Nav/>
+     <Hero/>
+    </>
+  )
+}
+
+export default App
